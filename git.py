39aeb7ea87512964e94871e929b4371419bf8c89
Aditya369-dot <adityabholla@gmail.com>
2020-09-18 15:38:14 -0700

    Virtual dictionary (rough draft)

--- git.py
+++ git.py
@@ -1,1 +1,18 @@
 print ("hello world")
+
+
+# creating my own dictionary 
+
+import json 
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    if w in data:
+        return data[w]
+    else:
+        return "the word doesnt exist. please double check it."
+
+word = input("enter word: ")
+
+print(translate(word))
